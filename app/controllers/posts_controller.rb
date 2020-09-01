534f3623661775756f6c104c67922999ee810333
karasuyamamika <j.girl.mi@gmail.com>
2020-09-01 22:12:02 +0900

    Revert "既読機能のフロントエンド実装"
    This reverts commit 15b1452190f10b76e73a96951ed464404e21885b.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,10 @@ class PostsController < ApplicationController
     if post.checked
       post.update(checked: false)
     else
-      post.update(checked: true)
+      post.checked(checked: true)
     end
 
     item = Post.find(params[:id])
-    render json: { post: item }
+    render json: { post: item}
   end
 end
